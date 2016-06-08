83e6b7cbc28c1c8f6ca63c4855607ee60a783a37
Armando Zamorano <zad93wlf@gmail.com>
2016-06-08 11:13:07 -0500

    structure

---
+++ comparative/app/src/main/java/com/globant/comparative/fragments/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.globant.comparative.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by zadtankus on 8/06/16.
+ */
+public class BaseFragment extends Fragment {
+}
---
+++ comparative/app/src/main/java/com/globant/comparative/fragments/FragmentList.java
@@ -0,0 +1,7 @@
+package com.globant.comparative.fragments;
+
+/**
+ * Created by zadtankus on 8/06/16.
+ */
+public class FragmentList {
+}
